c562a9f545cc2f69b3507eaae61cca768a7f8aff
giomamaca <gmama23@freeuni.edu.ge>
2026-06-14 20:43:33 +0000

    first experiment

--- src/data.py
+++ src/data.py
@@ -4,29 +4,43 @@ import numpy as np
 import pandas as pd
 
 class FERDataset(Dataset):
-    def __init__(self, df, augment=False):
+    def __init__(self, df, augment=False, has_labels=True):
         self.pixels = df['pixels'].values
-        self.labels = df['emotion'].values
+        self.has_labels = has_labels and ('emotion' in df.columns)
+        self.labels = df['emotion'].values if self.has_labels else None
         self.augment = augment
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.pixels)
 
     def __getitem__(self, idx):
-        img = np.fromstring(self.pixels[idx], dtype=np.uint8, sep=' ').reshape(48, 48)
+        img = np.array(self.pixels[idx].split(), dtype=np.uint8).reshape(48, 48)
         img = torch.tensor(img, dtype=torch.float32).unsqueeze(0) / 255.0
         if self.augment:
             if torch.rand(1) < 0.5:
                 img = torch.flip(img, dims=[2])
             i, j = torch.randint(0, 5, (2,))
             img = torch.nn.functional.pad(img[:, i:i+44, j:j+44], (2, 2, 2, 2))
-        return img, torch.tensor(self.labels[idx], dtype=torch.long)
+        if self.has_labels:
+            return img, torch.tensor(self.labels[idx], dtype=torch.long)
+        return img
 
-def get_loaders(csv_path, batch_size=64, augment_train=False):
-    df = pd.read_csv(csv_path)
-    train_ds = FERDataset(df[df['Usage'] == 'Training'], augment=augment_train)
-    val_ds   = FERDataset(df[df['Usage'] == 'PublicTest'])
-    test_ds  = FERDataset(df[df['Usage'] == 'PrivateTest'])
-    return (torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=2),
-            torch.utils.data.DataLoader(val_ds, batch_size=batch_size, num_workers=2),
-            torch.utils.data.DataLoader(test_ds, batch_size=batch_size, num_workers=2))
+def get_loaders(train_csv, batch_size=64, augment_train=False, val_ratio=0.1, seed=42):
+    df = pd.read_csv(train_csv)
+    rng = np.random.default_rng(seed)
+    val_idx = []
+    for c in df['emotion'].unique():
+        idx = df.index[df['emotion'] == c].to_numpy()
+        rng.shuffle(idx)
+        val_idx.extend(idx[:int(len(idx) * val_ratio)])
+    val_mask = df.index.isin(val_idx)
+    train_ds = FERDataset(df[~val_mask], augment=augment_train)
+    val_ds   = FERDataset(df[val_mask],  augment=False)
+    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True,  num_workers=2)
+    val_loader   = torch.utils.data.DataLoader(val_ds,   batch_size=batch_size, shuffle=False, num_workers=2)
+    return train_loader, val_loader
+
+def get_test_loader(test_csv, batch_size=64):
+    df = pd.read_csv(test_csv)
+    test_ds = FERDataset(df, augment=False, has_labels=False)
+    return torch.utils.data.DataLoader(test_ds, batch_size=batch_size, shuffle=False, num_workers=2)
